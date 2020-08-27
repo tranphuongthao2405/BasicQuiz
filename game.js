30e9385c6a2a9910a7d8ace2008b75dc5549e86a
tranphuongthao2405 <tranphuongthao24051999@gmail.com>
2020-08-27 18:58:11 +0700

    creat basic html and css

---
+++ game.js
@@ -0,0 +1,62 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswer = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestion = [];
+
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3,
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4,
+  },
+];
+
+// constants
+const CORRECT_BONUS = 10;
+const MAX_QUESTION = 3;
+
+const startGame = () => {
+    // reset all and get new question
+    questionCounter = 0;
+    score = 0;
+    availableQuestion = [...questions];
+    console.log(availableQuestion);
+    getNewQuestion();
+}
+
+const getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestion.length);
+    currentQuestion = availableQuestion[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];      
+    })
+}
+startGame();
